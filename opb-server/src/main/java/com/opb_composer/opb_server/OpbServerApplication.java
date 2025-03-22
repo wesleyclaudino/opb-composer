70d282f8e7232416f7cc7f82cc0a8c41b2be99e2
Wesley Claudino Rodrigues <claudinowesley11@gmail.com>
2025-03-22 17:47:59 -0300

    initial of backend opb-server

---
+++ opb-server/src/main/java/com/opb_composer/opb_server/OpbServerApplication.java
@@ -0,0 +1,13 @@
+package com.opb_composer.opb_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpbServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpbServerApplication.class, args);
+	}
+
+}
---
+++ opb-server/src/test/java/com/opb_composer/opb_server/OpbServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opb_composer.opb_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpbServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
